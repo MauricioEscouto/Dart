55408cc68761627c27f13aa6fa05a2373f538c97
Mauricio <mauricio@rodosoft.com.br>
2023-01-11 09:42:15 -0300

    concluído curso dart introdutório

--- curso1/bin/curso1.dart
+++ curso1/bin/curso1.dart
@@ -1,8 +1,19 @@
 void main(List<String> arguments) {
 
-  List<String> listanomes = ['Mauricio','Ricardo','Joao'];
-  print('Nomes: ${listanomes}!');
+  // List<String> listanomes = ['Mauricio','Ricardo','Joao'];
+  // print('Nomes: ${listanomes}!');
 
-  List<dynamic> pessoa = [20, 'Mauricio', 1.65, true];
-  print('Pessoa 1 - Nome: ${pessoa[1]}; Idade: ${pessoa[0]}; Altura ${pessoa[2]}; Cabelo preto? ${pessoa[3]}');
+  // List<dynamic> pessoa = [20, 'Mauricio', 1.65, true];
+  // print('Pessoa 1 - Nome: ${pessoa[1]}; Idade: ${pessoa[0]}; Altura ${pessoa[2]}; Cabelo preto? ${pessoa[3]}');
+
+  // const String apelido  = "teste";
+  int idade = 12;
+
+  if (idade >= 18){
+    print("maior de 18");
+  }
+  else {
+    print("menor de idade");
+  }
+  
 }
