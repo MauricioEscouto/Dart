051c8c65a2be4ce0c610f0a83eea1a4337a43d85
Mauricio <mauricio@rodosoft.com.br>
2023-01-24 09:24:45 -0300

    finalizado curso de dart em orientação a objeto

--- curso2/orientacao_objeto/bin/orientacao_objeto.dart
+++ curso2/orientacao_objeto/bin/orientacao_objeto.dart
@@ -7,20 +7,20 @@ void main(List<String> arguments) {
   fruta1.estaMadura();
 
   Uva uva1 = Uva("uva", "verde", 12.2, false, true);
-  
+  uva1.fazerMassa();
 }
                                   //nomeados opcionais
-mostrarMadura(String nome, int dias, {String? cor}) {
+void mostrarMadura(String nome, int dias, {String? cor}) {
     if (cor != null) {
       print("A $nome é $cor");
     }
   }
                                         //parametros padroes
-mostrarMadura1(String nome, int dias, {String cor = "sem cor"}) {
+void mostrarMadura1(String nome, int dias, {String cor = "sem cor"}) {
     print("A $nome é $cor");
   }
 
-class Fruta {
+class Fruta implements Bolo{
   String nome;
   String cor;
   double peso;
@@ -33,10 +33,37 @@ class Fruta {
       print("A fruta está madura");
     }
   }
+  
+  @override
+  void assar() {
+    print('catar a fruta');
+  }
+  
+  @override
+  void fazerMassa() {
+    print('misturar a fruta com farinha');
+  }
+  
+  @override
+  void separarIngredientes() {
+    print('assar o bolo');
+  }
 }
 
 class Uva extends Fruta{
   bool isAzedo;
 
   Uva (String nome, String cor, double peso, bool isMadura, this.isAzedo):super(nome, cor, peso, isMadura);
+
+  @override
+  void fazerMassa() {
+    print('\nTirar a casca e misturar com farinha');
+    super.fazerMassa();
+  }
+}
+
+abstract class Bolo {
+  void separarIngredientes();
+  void fazerMassa();
+  void assar();
 }
